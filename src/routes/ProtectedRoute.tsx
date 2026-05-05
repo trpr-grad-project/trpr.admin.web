d1d8cc90e86b459540e647d33999daee3f8c304f
Hager Hassan <hassanhager830@gmail.com>
2026-05-06 02:41:22 +0300

    temporarily disable protected routes for demo

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,9 +1,11 @@
-import { useSelector } from 'react-redux';
-import { Navigate, Outlet } from 'react-router-dom';
-import type { RootState } from '../store';
+import { Outlet } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
+// import { Navigate } from 'react-router-dom';
+// import type { RootState } from '../store';
 
 export default function ProtectedRoute() {
-  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
-
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  // const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  // return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
+  
+  return <Outlet />;
 }
