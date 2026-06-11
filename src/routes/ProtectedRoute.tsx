1d343c78c4de44933d14055dff1f77f1f4da5441
Hager Hassan <hassanhager830@gmail.com>
2026-06-11 17:22:05 +0300

    re-enable protected routes

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,11 +1,8 @@
-import { Outlet } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-// import { Navigate } from 'react-router-dom';
-// import type { RootState } from '../store';
+import { useSelector } from 'react-redux';
+import { Navigate, Outlet } from 'react-router-dom';
+import type { RootState } from '../store';
 
 export default function ProtectedRoute() {
-  // const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
-  // return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
-  
-  return <Outlet />;
+  const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 }
