2149b76bb0f56b1df54ef59e80c7f29c5099817d
OmarOsamaWT <omar.osama@tildetech.ae>
2026-06-25 03:06:06 +0300

    update internal endpiont

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'http://trpr.backend:5001',
+        target: 'http://trpr.backend:8080',
         changeOrigin: true,
       }
     }
